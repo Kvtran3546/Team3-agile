0738f3cb224378d4ef61ba31187d6bf9feb5acc7
Khoa <Kvtran3546@gmail.com>
2023-10-17 18:03:02 -0400

    testing completed

--- Town_Treasures/jest.config.cjs
+++ Town_Treasures/jest.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   transform: {
     '^.+\.(ts|tsx)$': 'ts-jest',
--- Town_Treasures/src/components/NavBar.tsx
+++ Town_Treasures/src/components/NavBar.tsx
@@ -1,70 +1,5 @@
-//import React, { useState } from 'react';
-// import { Navbar, Nav, Button } from 'react-bootstrap';
-// import { Link, useHistory } from 'react-router-dom';
-// import { useAuth } from "../contexts/AuthContext";
-//import "../App.css";
-//import axios from "axios";
 
-const NavbarComponent = () => {
-  //const [error, setError] = useState("")
-  //const { currentUser, logout } = useAuth();
-  //console.log(currentUser)
-
-  // async function handleLogout() {
-  //   setError("");
-  //   try {
-  //     await logout();
-  //     await axios.post('http://localhost:3001/logout');
-  //   } catch {
-  //     setError("Failed to log out");
-  //   }
-  
-  // }
-
-  // const showProfile = () => {
-  //   if (currentUser != null) {//user is signed in
-  //     return (
-  //       <div>
-  //         <Button className="custom" style={{marginRight: "15px"}} variant="primary" as={Link} to="/profile">
-  //           My profile
-  //         </Button>
-  //         <Button variant="outline-secondary" onClick={handleLogout}>
-  //           Log Out
-  //         </Button>
-  //       </div>
-  //     );
-  //   }
-  //   else {
-  //     return (
-  //       <div>
-  //         <Button variant="outline-primary" className="outline-custom" as={Link} to="/signup">Sign up</Button>
-  //         <Button variant="primary" as={Link} to="/login" className="ml-2 custom">Log in</Button>
-  //       </div>)
-  //   }
-  // }
-
-  // return (
-  //   <Navbar bg="light" expand="lg">
-  //     <Navbar.Brand as={Link} to="/">RecipEase</Navbar.Brand>
-  //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  //     <Navbar.Collapse id="basic-navbar-nav">
-  //       <Nav className="mr-auto">
-  //         <Nav.Link style={{color:"#727373"}} as={Link} to="/recipes">Home</Nav.Link>
-  //         {currentUser &&
-  //         <Nav.Link style={{color:"#727373"}} as={Link} to="/my-recipes">Explore</Nav.Link>
-  //         }
-  //         {currentUser &&
-  //         <Nav.Link style={{color:"#727373"}} as={Link} to="/my-likes">My Locations</Nav.Link>
-  //         }
-  //       </Nav>
-  //       <Nav>
-  //         {showProfile()}
-  //       </Nav>
-  //     </Navbar.Collapse>
-  //   </Navbar>
-  // )
-
-  
+const NavbarComponent:NavBar = () => {  
   return (
     <nav>
       <ul>
@@ -79,5 +14,11 @@ const NavbarComponent = () => {
   );
   
 }
+
+type NavBarType ={
+
+}
+
+type NavBar = (props: NavBarType) =>React.ReactElement
 
 export default NavbarComponent;
