d077cb1a8ef8b1fe84ddff145bb45a62ee030bdd
kimberlytsang <101619715+kimberlytsang@users.noreply.github.com>
2023-11-02 16:56:15 -0400

    Included the css style for Listings and added an extra link break in the website

--- Town_Treasures/src/pages/Listings.jsx
+++ Town_Treasures/src/pages/Listings.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import  '../css/Listings.css';
 import ListingCard from '../components/ListingCard.jsx';
 
 const Listings = () => {
@@ -22,11 +23,12 @@ const Listings = () => {
             image: 'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/302055401_408331531387955_90279840008017033_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=nJHhUd6X2okAX8RsoVb&_nc_ht=scontent-lga3-1.xx&oh=00_AfB3hSgIw3SdnISoXaIeHdnqTgRbxYZwa7tjfS2X6_GUEg&oe=65489945',
             title: 'Vitos Italian Deli',
             address: '806 Washington St',
-          }
+        }
     ]
     return (
         <div>
             <h1 className='text-black'>LISTINGS</h1>
+            <br></br>
             <div className="app">
                 {listingData.map((data, index) => (
                 <ListingCard key={index} {...data} />
