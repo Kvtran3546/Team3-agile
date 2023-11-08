a4f59e62b693d79f31c087cf0775f9771c3952d3
Khoa <ktran7@stevens.edu>
2023-11-08 17:26:33 -0500

    more changes to home page

--- Town_Treasures/src/components/SpecialButton.jsx
+++ Town_Treasures/src/components/SpecialButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SpecialButton = ({ text, imgSrc }) => {
   return (
-    <button className="relative flex items-center justify-center text-white font-bold rounded-md lg:text-[65px] lg:w-[400px] lg:h-[230px] md:text-[50px] md:w-[300px] mg:h-[190px] sm:text-[40px] sm:w-[250px] sm:h-[150px] hover:opacity-80 mx-5">
+    <button className="relative flex items-center justify-center text-black hover:text-white transition-all duration-500 font-bold rounded-md lg:text-[50px] lg:hover:text-[60px] lg:w-[400px] lg:h-[230px] md:text-[40px] md:hover:text-[50px] md:w-[300px] mg:h-[190px] sm:text-[30px] sm:hover:text-[20px] sm:w-[250px] sm:h-[150px] hover:opacity-80 mx-5">
       {imgSrc && <img src={imgSrc} alt="Button icon" className="absolute h-full w-full object-cover -z-10 overflow-hidden rounded-md" />}
       {text}
     </button>
--- Town_Treasures/src/pages/Home.jsx
+++ Town_Treasures/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Navbar, SearchBar, SpecialButton} from '../components'
 import { mainimg } from '../assets'
 import { special_buttons } from '../constants'
+import 'leaflet/dist/leaflet.css';
 const Home = () => {
   return (
     <div className='flex flex-col w-full justify-center items-center'>
@@ -11,12 +12,15 @@ const Home = () => {
           <img src={mainimg} alt="natureimg" className="absolute w-full h-full overflow-hidden object-cover -z-10 opacity-[90%] grayscale-[10%]"/>
         </div>
         <div className='flex flex-col justify-center items-center mt-10 w-[90%]'>
-          <h2 className='lg:text-[70px] text-gray-500 mb-5 md:text-[60px] sm:text-[50px] w-[100%] text-left'>Quick Searches</h2>
+          <h2 className='lg:text-[70px] text-gray-500 mb-10 md:text-[60px] sm:text-[50px] w-[100%] text-left'>Quick Searches</h2>
           <div className='flex flex-row'>
             {special_buttons.map((item) =>(
               <SpecialButton text={item.text} imgSrc={item.img}/>
             ))}
           </div>
+        </div>
+        <div className='flex flex-col justify-center items-center mt-10 w-[90%]'>
+          <h2 className='lg:text-[70px] text-gray-500 mb-5 md:text-[60px] sm:text-[50px] w-[100%] text-left'>Search for Nearby Spots</h2>
         </div>
     </div>
   )
