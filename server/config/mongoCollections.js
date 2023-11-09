89948090817c83dc7e950249e801ba17862c3db7
danieljkim1127 <danieljkim1127@gmail.com>
2023-11-09 17:52:17 -0500

    created server and user login/signup

---
+++ server/config/mongoCollections.js
@@ -0,0 +1,20 @@
+//import { dbConnection } from './mongoConnection.js';
+const dbConnection = require("./mongoConnection");
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection.dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+module.exports = {
+  users: getCollectionFn('users'),
+  listings: getCollectionFn('listings')
+};
---
+++ server/data/index.js
@@ -0,0 +1,8 @@
+const listingsDataFunctions = require("./listings.js");
+const userData = require("./users.js");
+
+module.exports = {
+    users: userData,
+    listingsData: listingsDataFunctions
+}
+
